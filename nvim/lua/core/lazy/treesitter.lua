df4aaf99741cf38be8d18b5d8aa83c03ff0cbee3
Ivan Turko <turkoivan0411@gmail.com>
2025-09-30 11:34:53 +0300

    nvim: enable treesitter for html and markdown

--- nvim/lua/core/lazy/treesitter.lua
+++ nvim/lua/core/lazy/treesitter.lua
@@ -29,23 +29,6 @@ return {
         indent = {
           enable = true,
         },
-
-        highlight = {
-          -- `false` will disable the whole extension
-          enable = true,
-          disable = function(lang, _)
-            if lang == "html" then
-              print "disabled"
-              return true
-            end
-          end,
-
-          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-          -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-          -- Using this option may slow down your editor, and you may see some duplicate highlights.
-          -- Instead of true it can also be a list of languages
-          additional_vim_regex_highlighting = { "markdown" },
-        },
       }
     end,
   },
