fa527b8b9b2c31938c3eeda70728593bf8ee9bdd
Ivan Turko <turkoivan0411@gmail.com>
2025-10-13 01:23:01 +0300

    feat(nvim): use Lua to detect IPython in .venv, remove shell redirect, warn if missing

--- nvim/lua/core/lazy/iron.lua
+++ nvim/lua/core/lazy/iron.lua
@@ -7,115 +7,119 @@ return {
     local iron = require "iron.core"
     local common = require "iron.fts.common"
 
-    local function get_poetry_ipython()
-      local handle = io.popen "poetry run which ipython 2>/dev/null"
-      if handle then
-        local result = handle:read("*a"):gsub("\n", "")
-        handle:close()
-        if result ~= "" then
-          return result
-        end
+    local function get_ipython_cmd()
+      local python = "./.venv/bin/python"
+
+      local handle = io.popen(string.format('%s -c "import IPython"', python))
+      local success = handle and handle:close()
+
+      if success then
+        return { python, "-m", "IPython", "--no-autoindent" }
+      else
+        vim.notify("IPython not found in this project's .venv", vim.log.levels.WARN)
+        return nil
       end
-      return "ipython" -- fallback
     end
 
-    local ipython_path = get_poetry_ipython()
+    local ipython_cmd = get_ipython_cmd()
 
-    iron.setup {
-      config = {
-        -- Whether a repl should be discarded or not
-        scratch_repl = true,
-        -- Your repl definitions come here
-        repl_definition = {
-          sh = {
-            -- Can be a table or a function that
-            -- returns a table (see below)
-            command = { "zsh" },
+    if ipython_cmd then
+      iron.setup {
+        config = {
+          -- Whether a repl should be discarded or not
+          scratch_repl = true,
+          -- Your repl definitions come here
+          repl_definition = {
+            sh = {
+              -- Can be a table or a function that
+              -- returns a table (see below)
+              command = { "zsh" },
+            },
+            python = {
+              command = ipython_cmd,
+              format = function(lines, extras)
+                local result = common.bracketed_paste_python(lines, extras)
+                local filtered = vim.tbl_filter(function(line)
+                  return not string.match(line, "^%s*#")
+                end, result)
+
+                if filtered[#filtered] and filtered[#filtered]:match "%S" then
+                  table.insert(filtered, "")
+                end
+
+                return filtered
+              end,
+              block_dividers = { "# %%", "#%%" },
+            },
           },
-          python = {
-            command = { ipython_path, "--no-autoindent" },
-            format = function(lines, extras)
-              local result = common.bracketed_paste_python(lines, extras)
-              local filtered = vim.tbl_filter(function(line)
-                return not string.match(line, "^%s*#")
-              end, result)
-
-              if filtered[#filtered] and filtered[#filtered]:match "%S" then
-                table.insert(filtered, "")
-              end
-
-              return filtered
-            end,
-            block_dividers = { "# %%", "#%%" },
-          },
+          -- set the file type of the newly created repl to ft
+          -- bufnr is the buffer id of the REPL and ft is the filetype of the
+          -- language being used for the REPL.
+          repl_filetype = function(_, ft)
+            return ft
+            -- or return a string name such as the following
+            -- return "iron"
+          end,
+          repl_open_cmd = "vertical split",
         },
-        -- set the file type of the newly created repl to ft
-        -- bufnr is the buffer id of the REPL and ft is the filetype of the
-        -- language being used for the REPL.
-        repl_filetype = function(_, ft)
-          return ft
-          -- or return a string name such as the following
-          -- return "iron"
-        end,
-        repl_open_cmd = "vertical split",
-      },
-      -- Iron doesn't set keymaps by default anymore.
-      -- You can set them here or manually add keymaps to the functions in iron.core
-      keymaps = {
-        toggle_repl = "<space>rr", -- toggles the repl open and closed.
-        -- If repl_open_command is a table as above, then the following keymaps are
-        -- available
-        -- toggle_repl_with_cmd_1 = "<space>rv",
-        -- toggle_repl_with_cmd_2 = "<space>rh",
-        restart_repl = "<space>rR", -- calls `IronRestart` to restart the repl
-        send_motion = "<space>sc",
-        visual_send = "<space>sc",
-        send_file = "<space>sf",
-        send_line = "<space>sl",
-        send_paragraph = "<space>sp",
-        send_until_cursor = "<space>su",
-        send_mark = "<space>sm",
-        send_code_block = "<space>sb",
-        send_code_block_and_move = "<space>sn",
-        mark_motion = "<space>mc",
-        mark_visual = "<space>mc",
-        remove_mark = "<space>md",
-        cr = "<space>s<cr>",
-        interrupt = "<space>s<space>",
-        exit = "<space>sq",
-        clear = "<space>cl",
-      },
-      -- If the highlight is on, you can change how it looks
-      -- For the available options, check nvim_set_hl
-      highlight = {
-        italic = true,
-      },
-      ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
-    }
+        -- Iron doesn't set keymaps by default anymore.
+        -- You can set them here or manually add keymaps to the functions in iron.core
+        keymaps = {
+          toggle_repl = "<space>rr", -- toggles the repl open and closed.
+          -- If repl_open_command is a table as above, then the following keymaps are
+          -- available
+          -- toggle_repl_with_cmd_1 = "<space>rv",
+          -- toggle_repl_with_cmd_2 = "<space>rh",
+          restart_repl = "<space>rR", -- calls `IronRestart` to restart the repl
+          send_motion = "<space>sc",
+          visual_send = "<space>sc",
+          send_file = "<space>sf",
+          send_line = "<space>sl",
+          send_paragraph = "<space>sp",
+          send_until_cursor = "<space>su",
+          send_mark = "<space>sm",
+          send_code_block = "<space>sb",
+          send_code_block_and_move = "<space>sn",
+          mark_motion = "<space>mc",
+          mark_visual = "<space>mc",
+          remove_mark = "<space>md",
+          cr = "<space>s<cr>",
+          interrupt = "<space>s<space>",
+          exit = "<space>sq",
+          clear = "<space>cl",
+        },
+        -- If the highlight is on, you can change how it looks
+        -- For the available options, check nvim_set_hl
+        highlight = {
+          italic = true,
+        },
+        ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
+      }
 
-    -- iron also has a list of commands, see :h iron-commands for all available commands
-    vim.keymap.set("n", "<space>rf", "<cmd>IronFocus<cr>")
-    vim.keymap.set("n", "<space>rh", "<cmd>IronHide<cr>")
+      -- iron also has a list of commands, see :h iron-commands for all available commands
+      vim.keymap.set("n", "<space>rf", "<cmd>IronFocus<cr>")
+      vim.keymap.set("n", "<space>rh", "<cmd>IronHide<cr>")
 
-    vim.keymap.set("n", "<C-x>", function()
-      local core = require "iron.core"
-      local ll = require "iron.lowlevel"
+      vim.keymap.set("n", "<C-x>", function()
+        local core = require "iron.core"
+        local ll = require "iron.lowlevel"
 
-      local ft = ll.get_buffer_ft(0)
-      if not ft then
-        return
-      end
+        local ft = ll.get_buffer_ft(0)
+        if not ft then
+          return
+        end
 
-      core.focus_on(ft)
+        core.focus_on(ft)
 
-      vim.defer_fn(function()
-        vim.cmd "startinsert"
-      end, 50)
-    end, { desc = "Iron: Focus REPL and enter insert mode" })
+        vim.defer_fn(function()
+          vim.cmd "startinsert"
+        end, 50)
+      end, { desc = "Iron: Focus REPL and enter insert mode" })
 
-    vim.keymap.set("t", "<C-x>", function()
-      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-\\><C-n>", true, false, true), "n", false)
-      vim.cmd "wincmd p"
-    end, { desc = "Iron: Exit terminal and focus previous window" })
+      vim.keymap.set("t", "<C-x>", function()
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-\\><C-n>", true, false, true), "n", false)
+        vim.cmd "wincmd p"
+      end, { desc = "Iron: Exit terminal and focus previous window" })
+    end
   end,
 }
