aa97536ef2b1912b8fa8dfda15410896707ea2c1
Ivan Turko <turkoivan0411@gmail.com>
2025-12-13 01:05:13 +0300

    feat(nvim): remap bindings in oil.nvim

--- nvim/lua/core/lazy/oil.lua
+++ nvim/lua/core/lazy/oil.lua
@@ -2,7 +2,12 @@ return {
   "stevearc/oil.nvim",
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    keymaps = {
+      ["xs"] = { "actions.select", opts = { vertical = true } },
+      ["xh"] = { "actions.select", opts = { horizontal = true } },
+    },
+  },
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
